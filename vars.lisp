f4cd2ebdddd600c46ba5bc3a2f07e3d3ea5ad799
D. Thompson <thompdump@gmail.com>
2011-08-02 10:18:15 -0700

    Remove unused model-item-properties variable.

--- vars.lisp
+++ vars.lisp
@@ -13,24 +13,6 @@
   "" ; "h"
   "Either NIL or a string representing the namespace for xhtml content. For example, 'h' would indicate that <p> should be rendered in an xhtml document as <h:p>.")
 
-;; for xforms-tables
-;; this should be redefined for application of interest
-(defparameter model-item-properties
-  (list
-   ;; table name and alist of property/value pairs
-   (cons "tests"
-	 (list
-	  (cons "curve" 
-		(list
-		 "readonly=\"boolean-from-string(if( ../curvelocked = 'T', 'true', 'false'))\"")
-		)
-	  (cons "curvetype" 
-		(list
-		 "readonly=\"boolean-from-string(if( ../curvelocked = 'T', 'true', 'false'))\"")
-		)
-	  )
-	 ))
-  "Model item properties (see xforms 1.1 section 6) for an item in a xforms model. Organized in a restrictive manner (for use with ssqlfs) as an alist with keys being 'table names' and values being alists with keys of 'column names'. Associated with a given table/columnlabel is a list of strings, 'propert=value' statement(s).")
 
 
 
