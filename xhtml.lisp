e6eb690944fd90f8b347a23e1f3a9f7fede78774
D Thompson <thompdump@gmail.com>
2011-10-17 21:32:38 -0700

    Add title attribute for all elements per HTML 4.01

--- xhtml.lisp
+++ xhtml.lisp
@@ -17,7 +17,7 @@
 ;;;     - generate larger structural block (e.g., HTML-TABLE, HTML-LIST
 ;;;
 
-(defun a-xhc (string &key attributes id href rwname rwval style stream target)
+(defun a-xhc (string &key attributes id href rwname rwval style stream target title)
   "Return <a> component of html document to stream STREAM. Rewrite URL with RWNAME (name) and RWVAL (value), if both are non-nil."
   (declare (string string))
   (let ((newhref
@@ -31,7 +31,9 @@
 	 :stream stream
 	 :attributes
 	 (list (list "href" newhref)
-	       (list "target" target)))))
+	       (list "target" target)
+	       (list "title" title)
+	       ))))
 
 (defun b-xhc (some-string &key attributes stream)
   (xhc "b" some-string :attributes attributes :stream stream))
@@ -44,7 +46,7 @@
 (defun br-xhc (&key attributes id stream)
   (xhc "br" nil :attributes attributes :id id :stream stream))
 
-(defun button-xhc (some-string &key accesskey class disabled id onblur onclick onfocus style stream tabindex type name value)
+(defun button-xhc (some-string &key accesskey class disabled id onblur onclick onfocus style stream tabindex title type name value)
   "Return a string."
   (xhc "button" some-string
        :attributes (list 
@@ -52,6 +54,7 @@
 		    (list "disabled" disabled)
 		    (list "name" name) 
 		    (list "tabindex" tabindex)
+		    (list "title" title)
 		    (list "type" type)
 		    (list "value" value) 
 		    (list "onclick" onclick)
@@ -87,7 +90,7 @@
   "Return as a string a <div>...</div> component of a xhtml document where SOME-STRING is included verbatim."
   (xhc "div" some-string :class class :id id :style style :stream stream))
 
-(defun form-xhc (some-string &key class enctype style stream action name method)
+(defun form-xhc (some-string &key class enctype style stream action name method title)
   "METHOD should be a keyword (e.g., :POST) corresponding to the form method attribute."
   (xhc "form"
        some-string
@@ -95,7 +98,9 @@
 		    (list "enctype" enctype)
 		    (list "method" (if method (string method)))
 		    (list "action" action)
-		    (list "name" name))
+		    (list "name" name)
+		    (list "title" title)
+		    )
        :class class
        :style style
        :stream stream))
@@ -166,16 +171,18 @@ If DEFAULTS-P is nil, don't include default xhtml content (see DEFAULT-HEAD-CONT
 (defun i-xhc  (some-string &key attributes class style stream)
   (xhc "i" some-string :attributes attributes :class class :style style :stream stream))
 
-(defun img-xhc (&key (alt "ALT not specified.") class src stream style)
+(defun img-xhc (&key (alt "ALT not specified.") class src stream style title)
   "Return a string."
   (xhc "img" ""
        :attributes (list (list "alt" alt)
-			 (list "src" src))
+			 (list "src" src)
+			 (list "title" title)
+			 )
        :class class
        :style style
        :stream stream))
 
-(defun input-xhc (&key accesskey checked class id onblur onfocus readonly style stream type name tabindex value)
+(defun input-xhc (&key accesskey checked class id onblur onfocus readonly style stream type name tabindex title value)
   (xhc "input" ""
        :attributes (list 
 		    (list "accesskey" accesskey)
@@ -184,6 +191,7 @@ If DEFAULTS-P is nil, don't include default xhtml content (see DEFAULT-HEAD-CONT
 		    (list "type" type)
 		    (list "name" name)
 		    (list "tabindex" tabindex)
+		    (list "title" title)
 		    (list "value" value)
 		    (list "onfocus" onfocus)
 		    (list "onblur" onblur))
@@ -195,23 +203,26 @@ If DEFAULTS-P is nil, don't include default xhtml content (see DEFAULT-HEAD-CONT
 (defun li-xhc  (some-string &key attributes class style stream)
   (xhc "li" some-string :attributes attributes :class class :style style :stream stream))
 
-(defun link-xhc (&key href type rel stream)
+(defun link-xhc (&key href type rel stream title)
   (xhc "link" 
        nil
        :attributes (list (list "href" href)
 			 (list "rel" rel)
+			 (list "title" title)
 			 (list "type" type))
        :stream stream))
 
 (defun ol-xhc  (some-string &key attributes class id style stream)
   (xhc "ol" some-string :attributes attributes :class class :id id :style style :stream stream))
 
-(defun option-xhc (some-string &key class label selected style stream value)
+(defun option-xhc (some-string &key class label selected style stream title value)
   (xhc "option" some-string 
         :attributes (list 
 		     (list "label" label)
 		     (list "value" value)
-		     (list "selected" selected))
+		     (list "selected" selected)
+		     (list "title" title)
+		     )
        :class class :style style :stream stream))
 
 (defun p-xhc (some-string &key attributes class style stream)
@@ -235,14 +246,16 @@ If DEFAULTS-P is nil, don't include default xhtml content (see DEFAULT-HEAD-CONT
 				   (list "src" src)
 				   (list "type" type))))
 
-(defun select-xhc (some-string &key accesskey class id multiple name onchange style stream tabindex)
+(defun select-xhc (some-string &key accesskey class id multiple name onchange style stream tabindex title)
   (xhc "select" some-string 
         :attributes (list 
 		     (list "accesskey" accesskey)
 		     (list "multiple" multiple)
 		     (list "name" name)
 		     (list "onchange" onchange)
-		     (list "tabindex" tabindex))
+		     (list "tabindex" tabindex)
+		     (list "title" title)		    
+		     )
        :class class :id id :style style :stream stream))
 
 (defun style-xhc (some-string &key protect-with-cdata-p stream (type "text/css"))
@@ -285,7 +298,7 @@ If DEFAULTS-P is nil, don't include default xhtml content (see DEFAULT-HEAD-CONT
 (defun td-xhc (some-string &key attributes stream)
   (xhc "td" some-string :attributes attributes :stream stream))
 
-(defun textarea-xhc (some-string &key accesskey class id onblur onfocus readonly style stream tabindex type name value)
+(defun textarea-xhc (some-string &key accesskey class id onblur onfocus readonly style stream tabindex title type name value)
   (xhc "textarea" some-string
        :attributes (list
 		    (list "accesskey" accesskey)
@@ -295,7 +308,9 @@ If DEFAULTS-P is nil, don't include default xhtml content (see DEFAULT-HEAD-CONT
 		    (list "tabindex" tabindex)
 		    (list "value" value)
 		    (list "onfocus" onfocus)
-		    (list "onblur" onblur))
+		    (list "onblur" onblur)
+		    (list "title" title)
+		    )
        :class class
        :id id
        :style style
